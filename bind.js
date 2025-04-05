cbf6760c028afd6bef6f5262028e106b470d7173
rokossovskaya <svnn175e@gmail.com>
2025-04-06 00:20:11 +0900

    set goal tomorrow

--- bind.js
+++ bind.js
@@ -1,6 +1,6 @@
 class Bind {
   constructor(options = {}) {
-    
+    // bind参数指定的element
   }
 }
 
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import Bind from "./bind";
 
+// 遍历document后绑定。
+// 新增的element怎么办 待思考。
+
 const bind = new Bind();
 
 class App {
