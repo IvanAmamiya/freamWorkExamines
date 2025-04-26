692b57812d9a86164e7ab8cddefb6565f93f8e58
刘体纯 <svnn175e@gmail.com>
2025-04-26 21:43:09 +0900

    fix bugs

--- BindComponent.js
+++ BindComponent.js
@@ -3,12 +3,11 @@ import Bind from './bind.js';
 export class BindComponent extends HTMLElement {
   constructor(bindOptions = {}) {
     super();
-    // 组件可选用 shadow DOM 或 light DOM
-    // 这里默认用 shadow DOM，如需 light DOM 可在子类中调整
     this.attachShadow({ mode: 'open' });
-    // 绑定数据，element 设为 shadowRoot
+    // 始终传递 component: this，保证不会报错
     this._bind = new Bind({
       ...bindOptions,
+      component: this,
       element: this.shadowRoot,
     });
   }
